823352e62464c73996d5c38155cfff05c03efee3
Mike Weidner <emw010@motorolasolutions.com>
2016-07-24 11:11:24 -0400

    add on target tests for vector3D and GravityObject

--- app/src/androidTest/java/com/example/emw010/gravitysim/AppGravityObjectTest.java
+++ app/src/androidTest/java/com/example/emw010/gravitysim/AppGravityObjectTest.java
@@ -41,4 +41,62 @@ public class AppGravityObjectTest extends ApplicationTestCase<Application> {
         assertEquals(1000, myGravityObjectA.m_mass, 1e-14 );
     }
 
+    public void test_GravityObject_relative() throws Exception
+    {
+        GravityObject myGravityObjectA = new GravityObject(
+                "Earth", // String name
+                1000, // double mass,
+                GravityObject.RECT,  // type
+                2345, 4567, 7890, // double pos_1, double pos_2, double pos_3,
+                12, 13, 14, // double vel_1, double vel_2, double vel_3,
+                0, // double angle,
+                null );
+        GravityObject myGravityObjectB = new GravityObject(
+                "Moon", // String name
+                2000, // double mass,
+                GravityObject.RECT,  // type
+                0.01, 0.02, 0.03, // double pos_1, double pos_2, double pos_3,
+                0.1, 0.2, 0.3, // double vel_1, double vel_2, double vel_3,
+                0, // double angle,
+                myGravityObjectA );
+
+        //
+        // make sure the base object is unchanged.
+        //
+        assertEquals("Earth", myGravityObjectA.m_name );
+        assertEquals(1000, myGravityObjectA.m_mass, 1e-14 );
+
+        assertEquals(2345, myGravityObjectA.m_pos.x, 2345*1e-14 );
+        assertEquals(4567, myGravityObjectA.m_pos.y, 4567*1e-14 );
+        assertEquals(0, myGravityObjectA.m_pos.z, 7890*1e-14 );  // note that thiis is zero because point must be on z axis prior to rotation.
+
+        assertEquals(12, myGravityObjectA.m_vel.x, 12*1e-14 );
+        assertEquals(13, myGravityObjectA.m_vel.y, 13*1e-14 );
+        assertEquals(14, myGravityObjectA.m_vel.z, 14*1e-14 );
+
+        assertEquals(0, myGravityObjectA.m_force.x, 0*1e-14 );
+        assertEquals(0, myGravityObjectA.m_force.y, 0*1e-14 );
+        assertEquals(0, myGravityObjectA.m_force.z, 0*1e-14 );
+
+
+        //
+        // make sure the relative stuff worked
+        //
+        assertEquals("Moon", myGravityObjectB.m_name );
+        assertEquals(2000, myGravityObjectB.m_mass, 1e-14 );
+
+        assertEquals(2345.01, myGravityObjectB.m_pos.x, 2345*1e-14);
+        assertEquals(4567.02, myGravityObjectB.m_pos.y, 4567*1e-14 );
+        assertEquals(0, myGravityObjectB.m_pos.z, 7890*1e-14 );
+
+        assertEquals(12.1, myGravityObjectB.m_vel.x, 12*1e-14 );
+        assertEquals(13.2, myGravityObjectB.m_vel.y, 13*1e-14 );
+        assertEquals(14.3, myGravityObjectB.m_vel.z, 14*1e-14 );
+
+        assertEquals(0, myGravityObjectB.m_force.x, 0*1e-14 );
+        assertEquals(0, myGravityObjectB.m_force.y, 0*1e-14 );
+        assertEquals(0, myGravityObjectB.m_force.z, 0*1e-14 );
+
+    }
+
 }
---
+++ app/src/androidTest/java/com/example/emw010/gravitysim/AppVector3DTest.java
@@ -0,0 +1,175 @@
+package com.example.emw010.gravitysim;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+//import org.junit.Test;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class AppVector3DTest extends ApplicationTestCase<Application> {
+    public AppVector3DTest() {
+        super(Application.class);
+    }
+
+
+
+    //    @Test
+    public void test_vector_constructor_zero() throws Exception
+    {
+        Vector3D myvector;
+        myvector = new Vector3D(0,0,0);
+
+        assertEquals(0, myvector.x, 0.000000000000001 );
+        assertEquals(0, myvector.y, 0.000000000000001 );
+        assertEquals(0, myvector.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_constructor_default() throws Exception
+    {
+        Vector3D myvector;
+        myvector = new Vector3D();
+
+        assertEquals(0, myvector.x, 0.000000000000001 );
+        assertEquals(0, myvector.y, 0.000000000000001 );
+        assertEquals(0, myvector.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_constructor_nonzero() throws Exception
+    {
+        Vector3D myvector;
+        myvector = new Vector3D(6.02e+23,3.14159265358979, 1.41421356237309);
+
+        assertEquals(6.02e+23, myvector.x, 10e+10 );
+        assertEquals(3.14159265358979, myvector.y, 0.000000000000001 );
+        assertEquals(1.41421356237309, myvector.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_set() throws Exception
+    {
+        Vector3D myvector;
+        myvector = new Vector3D(0,0,0);
+
+        myvector.set(1.1, 2.2, 3.3);
+
+        assertEquals(1.1, myvector.x, 0.000000000000001 );
+        assertEquals(2.2, myvector.y, 0.000000000000001 );
+        assertEquals(3.3, myvector.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_reset() throws Exception
+    {
+        Vector3D myvector;
+        myvector = new Vector3D(6.02e+23,3.14159265358979, 1.41421356237309);
+
+        myvector.reset();
+
+        assertEquals(0, myvector.x, 0.000000000000001 );
+        assertEquals(0, myvector.y, 0.000000000000001 );
+        assertEquals(0, myvector.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_add() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(1,2,3);
+        Vector3D myvectorB = new Vector3D(100,200,300);
+
+        myvectorA.add(myvectorB);
+
+        assertEquals(101, myvectorA.x, 0.000000000000001 );
+        assertEquals(202, myvectorA.y, 0.000000000000001 );
+        assertEquals(303, myvectorA.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_subtract() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(101,202,303);
+        Vector3D myvectorB = new Vector3D(100,200,300);
+
+        myvectorA.subtract(myvectorB);
+
+        assertEquals(1, myvectorA.x, 0.000000000000001 );
+        assertEquals(2, myvectorA.y, 0.000000000000001 );
+        assertEquals(3, myvectorA.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_getMagnitude() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(101,202,303);
+
+        double result = myvectorA.getMagnitude();
+
+        assertEquals(377.907396064, result, 0.000000001 );
+    }
+
+    //    @Test
+    public void test_vector_multiply() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(1,2,3);
+
+        myvectorA.multiply(10000);
+
+        assertEquals(10000, myvectorA.x, 0.00000000001 );
+        assertEquals(20000, myvectorA.y, 0.00000000001 );
+        assertEquals(30000, myvectorA.z, 0.00000000001 );
+    }
+
+    //    @Test
+    public void test_vector_divide() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(100,200,300);
+
+        myvectorA.divide(2);
+
+        assertEquals(50, myvectorA.x, 0.00000000001 );
+        assertEquals(100, myvectorA.y, 0.00000000001 );
+        assertEquals(150, myvectorA.z, 0.00000000001 );
+    }
+
+    //    @Test
+    public void test_vector_set_vector() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(1,2,3);
+        Vector3D myvectorB = new Vector3D(100,200,300);
+
+        myvectorA.set(myvectorB);
+
+        assertEquals(100, myvectorA.x, 0.000000000000001 );
+        assertEquals(200, myvectorA.y, 0.000000000000001 );
+        assertEquals(300, myvectorA.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_rotateAboutZ() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(1,0,0);
+
+        myvectorA.rotateAboutZ(90);
+
+        assertEquals(0, myvectorA.x, 0.000000000000001 );
+        assertEquals(1, myvectorA.y, 0.000000000000001 );
+        assertEquals(0, myvectorA.z, 0.000000000000001 );
+    }
+
+    //    @Test
+    public void test_vector_rotateAboutX() throws Exception
+    {
+        Vector3D myvectorA = new Vector3D(1,0,0);
+
+        myvectorA.rotateAboutX(90);
+
+        assertEquals(1, myvectorA.x, 0.000000000000001 );
+        assertEquals(0, myvectorA.y, 0.000000000000001 );
+        assertEquals(0, myvectorA.z, 0.000000000000001 );
+
+//        myvectorA.dump(1);
+    }
+
+}
