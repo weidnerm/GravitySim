eb4aca7d7dbb81f070fc4ed183cf323df6ddb891
Michael Weidner <michael.weidner@motorolasolutions.com>
2016-07-27 17:09:14 -0400

    test settings button.

--- app/src/androidTest/java/com/example/emw010/gravitysim/UiAutomationTest.java
+++ app/src/androidTest/java/com/example/emw010/gravitysim/UiAutomationTest.java
@@ -9,9 +9,11 @@ import org.junit.runner.RunWith;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Rect;
+import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
@@ -131,25 +133,80 @@ public class UiAutomationTest {
         catch(UiObjectNotFoundException e) {
             fail();
         }
-        UiObject2 changedText = mDevice.wait(Until.findObject(By.res(BASIC_SAMPLE_PACKAGE, "content")),500 /* wait 500ms */);
+
+        UiObject2 changedText = mDevice.wait(Until.findObject(By.text("Sun")),500);
         assertThat(changedText, notNullValue());
-//        assertThat("GravitySim", is(equalTo(changedText.getText())));
+        assertEquals("Sun", changedText.getText());
     }
 
-//    @Test
-//    public void testChangeText_newActivity() {
-//        // Type text and then press the button.
-//        mDevice.findObject(By.res(BASIC_SAMPLE_PACKAGE, "editTextUserInput"))
-//                .setText(STRING_TO_BE_TYPED);
-//        mDevice.findObject(By.res(BASIC_SAMPLE_PACKAGE, "activityChangeTextBtn"))
-//                .click();
-//
-//        // Verify the test is displayed in the Ui
-//        UiObject2 changedText = mDevice
-//                .wait(Until.findObject(By.res(BASIC_SAMPLE_PACKAGE, "show_text_view")),
-//                        500 /* wait 500ms */);
-//        assertThat(changedText.getText(), is(equalTo(STRING_TO_BE_TYPED)));
-//    }
+    @Test
+    public void testObjectSettingsButton() throws UiObjectNotFoundException {
+        mDevice.pressMenu();
+
+        UiObject aboutButton = mDevice.findObject(new UiSelector()
+                .text("Settings"));
+        assertThat(aboutButton, notNullValue());
+        assertEquals( true, aboutButton.exists() );
+
+        try {
+            aboutButton.click();
+        }
+        catch(UiObjectNotFoundException e) {
+            fail();
+        }
+
+        UiObject2 changedText = mDevice.wait(Until.findObject(By.text("3D Options")),500);
+        assertThat(changedText, notNullValue());
+
+        UiCollection settings = new UiCollection(new UiSelector()
+                .className("android.widget.ListView"));
+
+        int count = settings.getChildCount(new UiSelector()
+                .className("android.widget.CheckBox"));
+        assertEquals(4,count);
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(appContext);
+        assertThat(mySharedPreferences, notNullValue());
+        boolean m3dDisplay          = mySharedPreferences.getBoolean("enable_3d", false);
+        boolean mExcludeGreenColor  = mySharedPreferences.getBoolean("no_green", false);
+        boolean mAccelEnabled       = mySharedPreferences.getBoolean("enable_accel_tilt", false);
+        boolean mSeeGrid           = mySharedPreferences.getBoolean("see_grid", false);
+
+        UiObject myCheckbox0 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),0);
+        assertThat(myCheckbox0, notNullValue());
+        assertEquals(m3dDisplay,myCheckbox0.isChecked());  // make sure preference match gui
+        myCheckbox0.click();
+        assertEquals(m3dDisplay,mySharedPreferences.getBoolean("enable_3d", false) ? false: true);  // make sure preference is toggled
+        myCheckbox0.click();   // put it back to the original state
+
+        UiObject myCheckbox1 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),1);
+        assertThat(myCheckbox1, notNullValue());
+        assertEquals(mExcludeGreenColor,myCheckbox1.isChecked());  // make sure preference match gui
+        myCheckbox1.click();
+        assertEquals(mExcludeGreenColor,mySharedPreferences.getBoolean("no_green", false) ? false: true);  // make sure preference is toggled
+        myCheckbox1.click();   // put it back to the original state
+
+        UiObject myCheckbox2 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),2);
+        assertThat(myCheckbox2, notNullValue());
+        assertEquals(mSeeGrid,myCheckbox2.isChecked());  // make sure preference match gui
+        myCheckbox2.click();
+        assertEquals(mSeeGrid,mySharedPreferences.getBoolean("see_grid", false) ? false: true);  // make sure preference is toggled
+        myCheckbox2.click();   // put it back to the original state
+
+        UiObject myCheckbox3 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),3);
+        assertThat(myCheckbox3, notNullValue());
+        assertEquals(mAccelEnabled,myCheckbox3.isChecked());  // make sure preference match gui
+        myCheckbox3.click();
+        assertEquals(mAccelEnabled,mySharedPreferences.getBoolean("enable_accel_tilt", false) ? false: true);  // make sure preference is toggled
+        myCheckbox3.click();   // put it back to the original state
+
+
+
+    }
+
+
 
     /**
      * Uses package manager to find the package name of the device launcher. Usually this package
