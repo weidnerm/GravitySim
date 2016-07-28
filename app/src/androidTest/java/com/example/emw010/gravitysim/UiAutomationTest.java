b09d968caac0b12527116cb8c6bbdd3e682a35c1
Michael Weidner <michael.weidner@motorolasolutions.com>
2016-07-27 23:10:57 -0400

    add accelerometer disabled test

--- app/src/androidTest/java/com/example/emw010/gravitysim/UiAutomationTest.java
+++ app/src/androidTest/java/com/example/emw010/gravitysim/UiAutomationTest.java
@@ -7,12 +7,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Rect;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
@@ -44,6 +48,11 @@ public class UiAutomationTest {
     private static final String STRING_TO_BE_TYPED = "UiAutomator";
 
     private UiDevice mDevice;
+
+    final int CHECKBOX_INDEX_3D_ENABLE = 0;
+    final int CHECKBOX_INDEX_EXCLUDE_GREEN = 1;
+    final int CHECKBOX_INDEX_SEE_GRID = 2;
+    final int CHECKBOX_INDEX_ACCEL = 3;
 
     @Before
     public void startMainActivityFromHomeScreen() {
@@ -174,39 +183,94 @@ public class UiAutomationTest {
         boolean mAccelEnabled       = mySharedPreferences.getBoolean("enable_accel_tilt", false);
         boolean mSeeGrid           = mySharedPreferences.getBoolean("see_grid", false);
 
-        UiObject myCheckbox0 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),0);
+        UiObject myCheckbox0 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),CHECKBOX_INDEX_3D_ENABLE);
         assertThat(myCheckbox0, notNullValue());
         assertEquals(m3dDisplay,myCheckbox0.isChecked());  // make sure preference match gui
         myCheckbox0.click();
         assertEquals(m3dDisplay,mySharedPreferences.getBoolean("enable_3d", false) ? false: true);  // make sure preference is toggled
         myCheckbox0.click();   // put it back to the original state
 
-        UiObject myCheckbox1 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),1);
+        UiObject myCheckbox1 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),CHECKBOX_INDEX_EXCLUDE_GREEN);
         assertThat(myCheckbox1, notNullValue());
         assertEquals(mExcludeGreenColor,myCheckbox1.isChecked());  // make sure preference match gui
         myCheckbox1.click();
         assertEquals(mExcludeGreenColor,mySharedPreferences.getBoolean("no_green", false) ? false: true);  // make sure preference is toggled
         myCheckbox1.click();   // put it back to the original state
 
-        UiObject myCheckbox2 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),2);
+        UiObject myCheckbox2 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),CHECKBOX_INDEX_SEE_GRID);
         assertThat(myCheckbox2, notNullValue());
         assertEquals(mSeeGrid,myCheckbox2.isChecked());  // make sure preference match gui
         myCheckbox2.click();
         assertEquals(mSeeGrid,mySharedPreferences.getBoolean("see_grid", false) ? false: true);  // make sure preference is toggled
         myCheckbox2.click();   // put it back to the original state
 
-        UiObject myCheckbox3 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),3);
+        UiObject myCheckbox3 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),CHECKBOX_INDEX_ACCEL);
         assertThat(myCheckbox3, notNullValue());
         assertEquals(mAccelEnabled,myCheckbox3.isChecked());  // make sure preference match gui
         myCheckbox3.click();
         assertEquals(mAccelEnabled,mySharedPreferences.getBoolean("enable_accel_tilt", false) ? false: true);  // make sure preference is toggled
         myCheckbox3.click();   // put it back to the original state
-
-
-
     }
 
+//    @Test
+//    public void testAccelSlider() throws UiObjectNotFoundException, RemoteException {
+//        mDevice.pressMenu();
+//
+//        UiObject aboutButton = mDevice.findObject(new UiSelector()
+//                .text("Settings"));
+//        assertThat(aboutButton, notNullValue());
+//        aboutButton.click();
+//
+//        UiCollection settings = new UiCollection(new UiSelector()
+//                .className("android.widget.ListView"));
+//
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(appContext);
+//        assertThat(mySharedPreferences, notNullValue());
+//        boolean mAccelEnabled       = mySharedPreferences.getBoolean("enable_accel_tilt", false);
+//
+//        UiObject myCheckbox3 = settings.getChildByInstance(new UiSelector().className("android.widget.CheckBox"),CHECKBOX_INDEX_ACCEL);
+//        if ( mAccelEnabled != false)
+//        {
+//            myCheckbox3.click();  // toggle state
+//        }
+//
+//        mAccelEnabled       = mySharedPreferences.getBoolean("enable_accel_tilt", false);
+//        assertEquals(false, mAccelEnabled);
+//
+////        mDevice.setOrientationLeft();
+//        int width = mDevice.getDisplayWidth();
+//        int height = mDevice.getDisplayHeight();
+//
+//        Instrumentation mInstrumentation = InstrumentationRegistry.getInstrumentation();
+//        Instrumentation.ActivityMonitor monitor = mInstrumentation.addMonitor(Gravity.class.getName(),null,false);
+//
+//
+//        mInstrumentation.removeMonitor(monitor);
+////        Gravity mGravityObj = (Gravity)InstrumentationRegistry.getTargetContext();
+//        Gravity mGravityObj = (Gravity) monitor.getLastActivity();
+//        assertThat(mGravityObj, notNullValue());
+//        double startViewingAngle = mGravityObj.mViewingAngle;
+//
+//        mDevice.drag((width*9)/10, (height*1)/10, (width*9)/10, (height*9)/10, 50);// swipe for 50*5msec from (90%, 10%) to (90%,90%)
+//        double endViewingAngle = mGravityObj.mViewingAngle;
+//
+//        assertEquals(startViewingAngle, endViewingAngle);
+// mViewingAngle.
+//
+//    }
 
+//    public Activity getActivity() {
+//        Context context = getContext();
+//        while (context instanceof ContextWrapper) {
+//            if (context instanceof Activity) {
+//                return (Activity)context;
+//            }
+//            context = ((ContextWrapper)context).getBaseContext();
+//        }
+//        return null;
+//    }
 
     /**
      * Uses package manager to find the package name of the device launcher. Usually this package
